ed32306087c584e7c951074341bbe49579a69bd6
tianyu <tianyu@emindsoft.com.cn>
2019-08-13 13:05:33 +0800

    add role permission

---
+++ base-entity/src/main/java/cn/com/emindsoft/entity/po/RolePermission.java
@@ -0,0 +1,25 @@
+package cn.com.emindsoft.entity.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class RolePermission extends BaseEntity{
+    private String id;
+
+    private String roleId;
+
+    private String permissionId;
+
+    private String delFlag;
+
+    private Date createTime;
+
+    private String createBy;
+
+    private Date updateTime;
+
+    private String updateBy;
+
+}
